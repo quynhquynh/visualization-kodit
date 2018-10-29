ecdcd6475e1477f1ada46aaafb17396549f0df14
Quynh Le <q.lenhuquynh@gmail.com>
2018-10-29 16:47:19 +0200

    Updates

---
+++ precache-manifest.cb31bac3eb4b53ab95ce598e0506c4e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c140896165c098dd7b5d",
+    "url": "/kodit/static/js/runtime~main.c1408961.js"
+  },
+  {
+    "revision": "5244b794c132cfa0c12f",
+    "url": "/kodit/static/js/main.5244b794.chunk.js"
+  },
+  {
+    "revision": "0fa5b0aa112ecb1b1d3a",
+    "url": "/kodit/static/js/1.0fa5b0aa.chunk.js"
+  },
+  {
+    "revision": "5244b794c132cfa0c12f",
+    "url": "/kodit/static/css/main.498dfa80.chunk.css"
+  },
+  {
+    "revision": "a5aa71e41977815d604052457b61fff9",
+    "url": "/kodit/index.html"
+  }
+];
